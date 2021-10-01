da1dfc8accf7852cc364fc7a924bd6ff9044bfd7
samuel_yergen <samuel.yergen@hotmail.ch>
2021-10-01 13:27:05 +0200

    recup css

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
     <div className="App">
       <h1>Welcome to the Pfyn-Finges Forest!</h1>
 
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
